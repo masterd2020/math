aab4e6f43bd9f01f156f3b6bab4d4c2942a22609
Fasasi Toheeb <tohebayomide11@gmail.com>
2023-09-21 11:51:41 +0100

    feat: initial commit

---
+++ __tests__/index.js
@@ -0,0 +1,9 @@
+const root = require('../index.js');
+
+describe('Testing', () => {
+	it('should return a welcome message', () => {
+		const message = root.greet();
+
+		expect(message).toBe('Welcome');
+	});
+});
